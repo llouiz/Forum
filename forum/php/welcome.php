877539b4baed6475451625e6ed45f53306bf008d
Luiz Carlos <luizcarlos--1@hotmail.com>
2019-02-12 17:16:12 -0200

    Commitments

--- forum/php/welcome.php
+++ forum/php/welcome.php
@@ -1,54 +1,63 @@
-<link rel="stylesheet" type="text/css" href="../css/form.css">
-<?php session_start(); ?>
-	<header>
-	  <h1 id="legolas">Legolas</h1>
-	  <script src="../js/jquery-3.3.1.min.js"></script>
-	  <script src="../js/effect.js"></script>
-	</header>
-<div class="body content">
-	<a id="log-out" href="login.php">Logout</a>
-	<div class="welcome">
-	
-        <div id="mydiv" class="alert alert-success"><?= $_SESSION['message'] ?></div>
-        <!-- <span class="user"><img src='<?= $_SESSION['avatar'] ?>'></span><br/>
-        Welcome, <span class="user"><?= $_SESSION['username'] ?></span> -->
-		
-		<?php
-			$username = $_SESSION['username'];
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Welcome</title>
+		<meta charset="UTF-8">
+		<meta name="portview" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="../css/form.css">
+	</head>
+	<body>
+		<?php session_start(); ?>
+			<header>
+			  <h1 id="legolas">Legolas</h1>
+			  <script src="../js/jquery-3.3.1.min.js"></script>
+			  <script src="../js/effect.js"></script>
+			</header>
+		<div class="body content">
+			<a id="log-out" href="login.php">Logout</a>
+			<div class="welcome">
 			
-            $mysqli = new mysqli('localhost', 'root', 'snippetcode', 'accounts');
-			$sql = "SELECT username, avatar FROM users WHERE username='$username'";
-			$result = $mysqli->query($sql); //$result = mysqli_result object
+				<div id="mydiv" class="alert alert-success"><?= $_SESSION['message'] ?></div>
+				<!-- <span class="user"><img src='<?= $_SESSION['avatar'] ?>'></span><br/>
+				Welcome, <span class="user"><?= $_SESSION['username'] ?></span> -->
 				
-			while($row = $result->fetch_assoc()) {
+				<?php
+					$username = $_SESSION['username'];
+					
+					$mysqli = new mysqli('localhost', 'root', 'snippetcode', 'accounts');
+					$sql = "SELECT username, avatar FROM users WHERE username='$username'";
+					$result = $mysqli->query($sql); //$result = mysqli_result object
+						
+					while($row = $result->fetch_assoc()) {
+						
+						echo "<div><img src='$row[avatar]'><br/>";
+						echo "Welcome, <span class='user'>$row[username]</div></span>";
+						$_SESSION['avatar'] = $row['avatar'];
+					}
+				?>
 				
-				echo "<div><img src='$row[avatar]'><br/>";
-				echo "Welcome, <span class='user'>$row[username]</div></span>";
-				$_SESSION['avatar'] = $row['avatar'];
-			}
-        ?>
-		
-        <?php
-            $mysqli = new mysqli('localhost', 'root', 'snippetcode', 'accounts');
-            $sql = 'SELECT username, avatar FROM users';
-            $result = $mysqli->query($sql); //$result = mysqli_result object
-        ?>
-            
-        <div id="registered">
-            <span>All registered users</span>
-            <?php
-                while($row = $result->fetch_assoc()) {
-                    echo "<div class='userlist'><span>$row[username]</span><br/>";
-                    echo "<img src='$row[avatar]'></div>";
-                }
-            ?>
-        </div>
-    </div>
-</div>
+				<?php
+					$mysqli = new mysqli('localhost', 'root', 'snippetcode', 'accounts');
+					$sql = 'SELECT username, avatar FROM users';
+					$result = $mysqli->query($sql); //$result = mysqli_result object
+				?>
+					
+				<div id="registered">
+					<span>All registered users</span>
+					<?php
+						while($row = $result->fetch_assoc()) {
+							echo "<div class='userlist'><span>$row[username]</span><br/>";
+							echo "<img src='$row[avatar]'></div>";
+						}
+					?>
+				</div>
+			</div>
+		</div>
 
-<div class="body-content">
-	<footer>
-	  <p class="copyright">&copy; Legolas.com</p>
-    </footer>
-</div>
-    
+		<div class="body-content">
+			<footer>
+			  <p class="copyright">&copy; Legolas.com</p>
+			</footer>
+		</div>
+	</body>
+</html>
